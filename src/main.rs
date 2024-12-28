28b2aa058c2ee3a188ba9ec9ae33b6c32bec83e9
Drew Wyatt <drew.j.wyatt@gmail.com>
2024-12-28 13:06:20 -0500

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(index = 1, value_name = "DAY")]
+    day: u8,
+
+    #[arg(short, long, default_value_t = 0)]
+    part: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Day: {}", args.day);
+    println!("Part: {}", args.part);
 }
